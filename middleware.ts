c9e7707f5f130e3a1253dbbc89b56ac29421a34c
VIVEK MANGAL <138361918+vivekmangal204@users.noreply.github.com>
2025-12-28 05:22:26 +0000

    mr

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
+// import { NextResponse } from "next/server"
+// import type { NextRequest } from "next/server"
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
+// export function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl
 
-  // ✅ Login page ko hamesha allow karo
-  if (pathname === "/admin/login") {
-    return NextResponse.next()
-  }
+//   // ✅ Login page ko hamesha allow karo
+//   if (pathname === "/admin/login") {
+//     return NextResponse.next()
+//   }
 
-  // 🔒 Baaki admin routes protect karo
-  if (pathname.startsWith("/admin")) {
-    return NextResponse.redirect(
-      new URL("/admin/login", request.url)
-    )
-  }
+//   // 🔒 Baaki admin routes protect karo
+//   if (pathname.startsWith("/admin")) {
+//     return NextResponse.redirect(
+//       new URL("/admin/login", request.url)
+//     )
+//   }
 
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
-export const config = {
-  matcher: ["/admin/:path*"],
-}
+// export const config = {
+//   matcher: ["/admin/:path*"],
+// }
