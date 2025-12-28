5349eacc4923f5ffde5580f87410458c8df8f4e9
VIVEK MANGAL <138361918+vivekmangal204@users.noreply.github.com>
2025-12-28 05:15:14 +0000

    fix layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,33 +5,40 @@ import { auth } from "@/lib/firebase"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
+// export default function AdminLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   const router = useRouter()
+//   const [loading, setLoading] = useState(true)
+
+//   useEffect(() => {
+//     const unsub = onAuthStateChanged(auth, (user) => {
+//       if (!user) {
+//         router.replace("/admin/login")
+//       } else {
+//         setLoading(false)
+//       }
+//     })
+
+//     return () => unsub()
+//   }, [router])
+
+//   if (loading) {
+//     return (
+//       <div className="h-screen flex items-center justify-center">
+//         Checking authentication...
+//       </div>
+//     )
+//   }
+
+//   return <>{children}</>
+// }
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const router = useRouter()
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => {
-      if (!user) {
-        router.replace("/admin/login")
-      } else {
-        setLoading(false)
-      }
-    })
-
-    return () => unsub()
-  }, [router])
-
-  if (loading) {
-    return (
-      <div className="h-screen flex items-center justify-center">
-        Checking authentication...
-      </div>
-    )
-  }
-
   return <>{children}</>
 }
